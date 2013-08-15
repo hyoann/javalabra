656a265ec251e4eb59e021f753a129ee8b6642f2
Annamaria Hyotylainen <annamaria.hyotylainen@helsinki.fi>
2013-08-15 21:39:25 +0300

    lisatty sanavalitsin-luokka

---
+++ sanaohjelma/src/sanaohjelma/sovelluslogiikka/Sanavalitsin.java
@@ -0,0 +1,26 @@
+package sanaohjelma.sovelluslogiikka;
+
+public class Sanavalitsin {
+    private int toistovali; //kuinka monen sanan välein kysytään väärin arvattuja sanoja
+    private Sanat sanat;
+    private Tilasto tilasto;
+
+    public Sanavalitsin(int vali, Tilasto tilasto, Sanat sanat) {
+        this.toistovali = vali;
+        this.sanat = sanat;
+        this.tilasto = tilasto;
+    }
+
+    public String annaSana(String kieli) {
+        tilasto.kasvataSanamaaraa();
+    
+        //onko aika  kysyä sanaa mokattujen sanojen listalta
+        if (tilasto.sanamaara() != 0 && tilasto.sanamaara() % this.toistovali == 0) {
+            String kysyttavaSana = tilasto.annaMuistio(kieli).annaJokuSana();
+            if (kysyttavaSana != null) {
+                    return kysyttavaSana;
+            }
+        }
+            return this.sanat.annaJokuSana(kieli);
+    }
+}
